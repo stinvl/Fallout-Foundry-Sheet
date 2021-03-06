fd8453b97e7fd2ce438f0b502514039cb11730cf
Stinvl <jamescowen7@gmail.com>
2021-03-06 20:03:02 +0000

    x\xz\z
    z\xz\

---
+++ src/scripts/app/show-actor-art.js
@@ -0,0 +1,34 @@
+export const tidy5eShowActorArt = function (html,actor){
+  let portrait = html.find('.portrait'),
+      portraitMenu = html.find('.portrait-menu'),
+      portraitButton = html.find('.showActorArt');
+
+  portrait.mousedown( async (e) => {
+    switch (e.which){
+      case 3:
+        portraitMenu.toggleClass('hidden');
+        break;
+    }
+  })
+
+  portraitButton.click( function(e){
+    e.preventDefault();
+    portraitMenu.addClass('hidden');
+    let id = $(this).attr('id'),
+        portraitImg = actor.data.img,
+        tokenImg = actor.data.token.img
+    if (id == 'showPortrait'){
+      new ImagePopout(portraitImg, {
+				title: 'Portrait: '+actor.name,
+				shareable: true,
+				uuid: actor.uuid
+			}).render(true);
+    } else {
+      new ImagePopout(tokenImg, {
+				title: 'Token: '+actor.name,
+				shareable: true,
+				uuid: actor.uuid
+			}).render(true);
+    }
+  })
+}
